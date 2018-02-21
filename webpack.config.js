c41426a6111f9e4e161ed8402e299a640ee175e3
Nicolas Novello <nico.novello@gmail.com>
2018-02-21 13:40:05 +0100

    add watch and hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
   entry: './src/app.js',
   plugins: [
     new CleanWebpackPlugin(['dist'], {
       exclude: ['index.html']
-    })
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist',
+    host: "0.0.0.0",
+    hot: true
+  },
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist')
