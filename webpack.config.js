07b4c59679d784b7a5760da0e88cd0df9364e617
Nicolas Novello <nico.novello@gmail.com>
2018-02-21 13:05:05 +0100

    modify config to keep one bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-  entry: {
-    app: './src/app.js'
-  },
+  entry: './src/app.js',
   plugins: [
     new CleanWebpackPlugin(['dist'], {
       exclude: ['index.html']
     })
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'app.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
